b977685f0efd3f97876594499c591ed17510c205
Leonardo1041 <leonardo.pavan@fatec.sp.gov.br>
2025-08-20 13:53:23 +0000

    (20/08)

---
+++ back-end/src/database/client.js
@@ -0,0 +1,17 @@
+import { PrismaClient } from '../generated/prisma/default.js'
+
+const prisma = new PrismaClient({
+  log: [{
+    emit: 'event',
+    level: 'query'
+  }]
+})
+
+// Exibe no terminal as instruções SQL enviadas ao BD
+prisma.$on('query', event => {
+  console.log('-'.repeat(60))
+  console.log(event.query)
+  if(event.params) console.log('PARAMS:', event.params)
+})
+
+export default prisma
