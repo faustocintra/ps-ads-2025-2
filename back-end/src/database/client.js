f3a228c53455593c4f811c5cee4cccf430ecd461
Victor Hugo <141638615+VictorHugo016@users.noreply.github.com>
2025-08-27 13:39:52 +0000

    (27/08) ENTREGA DA ATIVIDADE 1

--- back-end/src/database/client.js
+++ back-end/src/database/client.js
@@ -1,17 +1,17 @@
-import {PrismaClient} from '../generated/prisma/default.js'
+import { PrismaClient } from '../generated/prisma/default.js'
 
-const prisma = new PrismaClient ({
-    log: [{
-        emit: 'event',
-        level: 'query'
-    }]
+const prisma = new PrismaClient({
+  log: [{
+    emit: 'event',
+    level: 'query'
+  }]
 })
 
-// exibe no terminal as instruções SQL enviadas ao BD
+// Exibe no terminal as instruções SQL enviadas ao BD
 prisma.$on('query', event => {
-    console.log('-'.repeat(60))
-    console.log(event.query)
-    if(event.params) console.log('PARAMS:', event.params)
+  console.log('-'.repeat(60))
+  console.log(event.query)
+  if(event.params) console.log('PARAMS:', event.params)
 })
 
 export default prisma
