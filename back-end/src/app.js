0dfbda951fed0ff5f814ab70c08fba6f588a295c
Jonatas Candido Gonçalves <jonatascandido364@gmail.com>
2025-09-03 12:00:27 +0000

    (03/09) ENTREGA DA ATIVIDADE 2

--- back-end/src/app.js
+++ back-end/src/app.js
@@ -4,20 +4,21 @@ import logger from 'morgan'
 
 import indexRouter from './routes/index.js'
 import usersRouter from './routes/users.js'
+import customersRoute from './routes/customers.js'
+import carsRoute from './routes/cars.js'
 
 const app = express()
 
+// Middlewares
 app.use(logger('dev'))
 app.use(json())
 app.use(urlencoded({ extended: false }))
 app.use(cookieParser())
 
+/**************** ROTAS *******************/
 app.use('/', indexRouter)
 app.use('/users', usersRouter)
-
-/**************** ROTAS *******************/
-
-import customersRoute from './routes/customers.js'
 app.use('/customers', customersRoute)
+app.use('/cars', carsRoute)
 
 export default app
