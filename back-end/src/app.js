1819ef7a85b9b5ba045fb2119aa0403c9ca5ef7a
Dalila Bueno <dalila.batista@fatec.sp.gov.br>
2025-09-03 13:16:42 +0000

    (03/09) ENTREGA DA ATIVIDADE 2

--- back-end/src/app.js
+++ back-end/src/app.js
@@ -1,23 +1,28 @@
 import express, { json, urlencoded } from 'express'
 import cookieParser from 'cookie-parser'
 import logger from 'morgan'
-
+ 
 import indexRouter from './routes/index.js'
 import usersRouter from './routes/users.js'
-
+ 
+ 
 const app = express()
-
+ 
 app.use(logger('dev'))
 app.use(json())
 app.use(urlencoded({ extended: false }))
 app.use(cookieParser())
-
+ 
 app.use('/', indexRouter)
 app.use('/users', usersRouter)
-
-/******************ROTAS******************/
-
+ 
+ 
+/**************** ROTAS *******************/
+ 
 import customersRoute from './routes/customers.js'
 app.use('/customers', customersRoute)
-
+ 
+import carsRouter from './routes/cars.js'
+app.use('/cars', carsRouter)
+ 
 export default app
