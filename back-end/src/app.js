e86bb92c31e666d05f0d6cc3ba07860b42e7f6d5
Prof. Fausto Cintra <professor@faustocintra.com.br>
2025-09-10 12:31:32 +0000

    (10/09) Criação da tabela de usuários e criptografia de senhas

--- back-end/src/app.js
+++ back-end/src/app.js
@@ -3,7 +3,6 @@ import cookieParser from 'cookie-parser'
 import logger from 'morgan'
 
 import indexRouter from './routes/index.js'
-import usersRouter from './routes/users.js'
 
 const app = express()
 
@@ -13,11 +12,16 @@ app.use(urlencoded({ extended: false }))
 app.use(cookieParser())
 
 app.use('/', indexRouter)
-app.use('/users', usersRouter)
 
 /**************** ROTAS *******************/
 
 import customersRoute from './routes/customers.js'
 app.use('/customers', customersRoute)
+
+import carsRoute from './routes/cars.js'
+app.use('/cars', carsRoute)
+
+import usersRoute from './routes/users.js'
+app.use('/users', usersRoute)
 
 export default app
