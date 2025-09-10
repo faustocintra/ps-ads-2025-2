bd4ee0c96aba4bd6f15fa0c410844334ede35a26
Marcos Vinícius <marcos.pavane@fatec.sp.gov.br>
2025-09-10 12:23:52 +0000

    (10/09) Criação da tabela de usuários e criptografia de senhas

--- back-end/prisma/migrations/20250910105036_create_users/migration.sql
+++ back-end/prisma/migrations/20250910105036_create_users/migration.sql
@@ -30,3 +30,21 @@ CREATE TABLE "public"."Car" (
 
     CONSTRAINT "Car_pkey" PRIMARY KEY ("id")
 );
+
+-- CreateTable
+CREATE TABLE "public"."User" (
+    "id" SERIAL NOT NULL,
+    "fullname" TEXT NOT NULL,
+    "username" TEXT NOT NULL,
+    "email" TEXT NOT NULL,
+    "password" TEXT NOT NULL,
+    "is_admin" BOOLEAN NOT NULL DEFAULT false,
+
+    CONSTRAINT "User_pkey" PRIMARY KEY ("id")
+);
+
+-- CreateIndex
+CREATE UNIQUE INDEX "User_username_key" ON "public"."User"("username");
+
+-- CreateIndex
+CREATE UNIQUE INDEX "User_email_key" ON "public"."User"("email");
