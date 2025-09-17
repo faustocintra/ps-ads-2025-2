58d8af303f07f42c4515dfbd9b3e2096fc64332e
Valfuentes-24 <valquiriafuentes@hotmail.com>
2025-09-17 11:51:49 +0000

    (10/09) Criação da tabela de usuários e criptografia de senhas

--- back-end/src/app.js
+++ back-end/src/app.js
@@ -3,7 +3,6 @@ import cookieParser from 'cookie-parser'
 import logger from 'morgan'
 
 import indexRouter from './routes/index.js'
-import usersRouter from './routes/users.js'
 
 
 const app = express()
@@ -14,7 +13,6 @@ app.use(urlencoded({ extended: false }))
 app.use(cookieParser())
 
 app.use('/', indexRouter)
-app.use('/users', usersRouter)
 
 /******* ROTAS ********/
 import customersRouter from './routes/customers.js'
@@ -23,5 +21,7 @@ app.use('/customers', customersRouter)
 import carsRoute from './routes/cars.js'
 app.use('/cars', carsRoute)
 
+import usersRoute from './routes/users.js'
+app.use('/users', usersRoute)
 
 export default app
---
+++ back-end/src/controllers/users.js
@@ -0,0 +1,149 @@
+import prisma from '../database/client.js'
+import bcrypt from 'bcrypt'
+
+
+const controller = {}   // Objeto vazio
+
+// Todas as funções de controller têm, pelo menos,
+// dois parâmetros:
+// req ~> representa a requisição (request)
+// res ~> representa a resposta (response)
+controller.create = async function(req, res) {
+  try {
+    // Se existe o campo 'password' em req.body,
+    	// é necessário gerar o hash da senha antes
+    	// de armazená-lo no BD. 12 é a quantidade de passos de criptografia
+    	if(req.body.password) {
+       req.body.password = await bcrypt.hash(req.body.password, 12)
+    	}
+
+    // Para a inserção no BD, os dados são enviados
+    // dentro de um objeto chamado "body" que vem
+    // dentro da requisição ("req")
+    await prisma.user.create({ data: req.body })
+
+    // Se tudo der certo, enviamos o código HTTP
+    // apropriado, no caso
+    // HTTP 201: created
+    res.status(201).end()
+  }
+  catch(error) {
+    // Se algo de errado ocorrer, cairemos aqui
+    console.error(error)  // Exibe o erro no terminal
+
+    // Enviamos como resposta o código HTTP relativo
+    // a erro interno do servidor
+    // HTTP 500: Internal Server Error
+    res.status(500).end()
+  }
+}
+
+controller.retrieveAll = async function(req, res) {
+  try {
+    // Recupera todos os registros de clientes, ordenados
+    // pelo campo "name"
+    const result = await prisma.user.findMany({
+      orderBy: [ { fullname: 'asc' } ]
+    })
+
+    // HTTP 200: OK (implícito)
+    res.send(result)
+  }
+  catch(error) {
+    // Se algo de errado ocorrer, cairemos aqui
+    console.error(error)  // Exibe o erro no terminal
+
+    // Enviamos como resposta o código HTTP relativo
+    // a erro interno do servidor
+    // HTTP 500: Internal Server Error
+    res.status(500).end()
+  }
+}
+
+controller.retrieveOne = async function (req, res) {
+  try {
+    // Busca no banco de dados apenas o cliente indicado
+    // pelo parâmetro "id"
+    const result = await prisma.user.findUnique({
+      where: { id: Number(req.params.id) }
+    })
+
+    // Encontrou ~> HTTP 200: OK (implícito)
+    if(result) res.send(result)
+    // Não encontrou ~> HTTP 404: Not Found
+    else res.status(404).end()
+  }
+  catch(error) {
+    // Se algo de errado ocorrer, cairemos aqui
+    console.error(error)  // Exibe o erro no terminal
+
+    // Enviamos como resposta o código HTTP relativo
+    // a erro interno do servidor
+    // HTTP 500: Internal Server Error
+    res.status(500).end()
+  }
+}
+
+controller.update = async function(req, res) {
+  try {
+    // Se existe o campo 'password' em req.body,
+    	// é necessário gerar o hash da senha antes
+    	// de armazená-lo no BD. 12 é a quantidade de passos de criptografia
+    	if(req.body.password) {
+       req.body.password = await bcrypt.hash(req.body.password, 12)
+    	}
+
+    // busaca o registro no bd por seu id
+    // e atualiza com as informações que viera, em req.body
+    await prisma.user.update({
+      where: { id: Number(req.params.id) },
+      data: req.body
+    })
+
+    // Se tudo der certo, enviamos o código HTTP
+    // apropriado, no caso
+    // HTTP 204: No Content
+    res.status(204).end()
+  }
+  catch(error) {
+    // Se algo de errado ocorrer, cairemos aqui
+    console.error(error)  // Exibe o erro no terminal
+
+    // não encontrou e não atualizou
+    if(error?.code === 'P2025') res.status(404).end()
+      return
+    }
+
+    // Enviamos como resposta o código HTTP relativo
+    // a erro interno do servidor
+    // HTTP 500: Internal Server Error
+    res.status(500).end()
+  }
+
+  controller.delete = async function(req, res) {
+    try {
+      // busca o registro no bd por seu id
+      await prisma.user.delete({
+        where: { id: Number(req.params.id) }
+      })
+
+      // encontrou e excluiu  -> HTTP 204: No Content
+      res.status(204).end()
+
+    }
+    catch(error) {
+      // Se algo de errado ocorrer, cairemos aqui
+      console.error(error)  // Exibe o erro no terminal
+
+      // não encontrou e não atualizou
+      if(error?.code === 'P2025') res.status(404).end()
+        return
+      }
+
+      // Enviamos como resposta o código HTTP relativo
+      // a erro interno do servidor
+      // HTTP 500: Internal Server Error
+      res.status(500).end()
+    }
+
+export default controller
