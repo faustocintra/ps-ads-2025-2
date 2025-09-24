36b0b9f1c4f9a3556273cb75fbdaaaaae48a442e
annaclaramaia <annaclaramaiaa@gmail.com>
2025-09-24 11:36:37 +0000

    Prova 1

---
+++ back-end/prisma/migrations/20250924105803_create_sellers/migration.sql
@@ -0,0 +1,18 @@
+-- CreateTable
+CREATE TABLE "public"."Seller" (
+    "id" SERIAL NOT NULL,
+    "fullname" TEXT NOT NULL,
+    "birth_date" TIMESTAMP(3),
+    "identity_document" TEXT NOT NULL,
+    "email" TEXT NOT NULL,
+    "phone" TEXT NOT NULL,
+    "is_manager" BOOLEAN NOT NULL DEFAULT false,
+
+    CONSTRAINT "Seller_pkey" PRIMARY KEY ("id")
+);
+
+-- CreateIndex
+CREATE UNIQUE INDEX "Seller_identity_document_key" ON "public"."Seller"("identity_document");
+
+-- CreateIndex
+CREATE UNIQUE INDEX "Seller_email_key" ON "public"."Seller"("email");
