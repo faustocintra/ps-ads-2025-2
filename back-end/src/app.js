f63afa81071d6db6c7a845d0a20a5e39f26e6b1f
Dalila Bueno <dalila.batista@fatec.sp.gov.br>
2025-09-24 12:38:40 +0000

    (24/09) ENTREGA DA PROVA 1

--- back-end/src/app.js
+++ back-end/src/app.js
@@ -28,4 +28,7 @@ app.use('/cars', carsRouter)
 import usersRouter from './routes/users.js'
 app.use('/users', usersRouter)
  
+import sellersRouter from './routes/sellers.js'
+app.use('/sellers', sellersRouter)
+ 
 export default app
