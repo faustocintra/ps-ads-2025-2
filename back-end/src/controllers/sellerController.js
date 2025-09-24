e23e4b7b0ee9e1e5d7e03ace32b00b9557aca6f6
Jonatas Candido Gonçalves <jonatascandido364@gmail.com>
2025-09-24 12:30:12 +0000

    PROVA 1 - Jonatas Candido goncalves

---
+++ back-end/src/controllers/sellerController.js
@@ -0,0 +1,88 @@
+import prisma from '../database/client.js'
+
+const controller = {}   
+
+controller.create = async function(req, res) {
+  try {
+    const { fullname, birth_date, identity_document, email, phone, is_manager } = req.body
+    
+    await prisma.seller.create({
+      data: {
+        fullname,
+        birth_date: new Date(birth_date),
+        identity_document,
+        email,
+        phone,
+        is_manager: is_manager || false,
+      },
+    })
+
+    res.status(201).end()
+  }
+  catch(error) {
+    console.error(error)
+    res.status(500).end()
+  }
+}
+
+controller.retrieveAll = async function(req, res) {
+  try {
+    const result = await prisma.seller.findMany({
+      orderBy: [ { fullname: 'asc' } ]
+    })
+
+    res.send(result)
+  }
+  catch(error) {
+    console.error(error)
+    res.status(500).end()
+  }
+}
+
+controller.retrieveOne = async function (req, res) {
+  try {
+    const result = await prisma.seller.findUnique({
+      where: { id: Number(req.params.id) }
+    })
+
+    if(result) res.send(result)
+    else res.status(404).end()
+  }
+  catch(error) {
+    console.error(error)
+    res.status(500).end()
+  }
+}
+
+controller.update = async function(req, res) {
+  try {
+    await prisma.seller.update({
+      where: { id: Number(req.params.id) },
+      data: req.body
+    })
+
+    res.status(204).end()
+  }
+  catch(error) {
+    console.error(error)
+    if(error?.code === 'P2025') res.status(404).end()
+    else res.status(500).end()
+  }
+}
+
+controller.delete = async function (req, res) {
+  try {
+    await prisma.seller.delete({
+      where: { id: Number(req.params.id) }
+    })
+
+    res.status(204).end()
+  }
+  catch(error) {
+    console.error(error)
+    if(error?.code === 'P2025') res.status(404).end()
+    else res.status(500).end()
+  }
+}
+
+export default controller
---
+++ back-end/src/routes/sellerRouter.js
@@ -0,0 +1,12 @@
+import express from 'express'
+import controller from '../controllers/sellerController.js'
+
+const router = express.Router()
+
+router.post('/', controller.create)
+router.get('/', controller.retrieveAll)
+router.get('/:id', controller.retrieveOne)
+router.put('/:id', controller.update)
+router.delete('/:id', controller.delete)
+
+export default router
