17d1b29ccf50d58f98016a9d90964a1de397f16b
L1711 <leilanecalves@outlook.com>
2025-10-01 13:20:25 +0000

    (01/10)- Implementando login com certificado token

--- back-end/src/app.js
+++ back-end/src/app.js
@@ -1,3 +1,6 @@
+// Carregando as variáveis de ambiente do arquivo.env
+import dotenv from 'dotenv'
+dotenv.config()
 import express, { json, urlencoded } from 'express'
 import cookieParser from 'cookie-parser'
 import logger from 'morgan'
@@ -14,10 +17,13 @@ app.use(cookieParser())
 app.use('/', indexRouter)
 
 /***************ROTAS***************/
+// Middleware de verificação de autorização
+import authMiddleware from './middleware/auth.js'
+app.use(authMiddleware)
 import customersRoute from './routes/customers.js'
 import carsRoute from './routes/cars.js'
 import usersRoute from './routes/users.js'
-import sellersRoute from './routes/users.js'
+import sellersRoute from './routes/sellers.js'
 app.use('/customers', customersRoute)
 app.use('/cars', carsRoute)
 app.use('/users', usersRoute)
--- back-end/src/controllers/users.js
+++ back-end/src/controllers/users.js
@@ -1,3 +1,4 @@
+import jwt from 'jsonwebtoken'
 import prisma from '../database/client.js'
 import bcrypt from 'bcrypt'
 
@@ -19,7 +20,7 @@ controller.create = async function(req, res) {
     // Para a inserção no BD, os dados são enviados
     // dentro de um objeto chamado "body" que vem
     // dentro da requisição ("req")
-    await prisma.seller.create({ data: req.body })
+    await prisma.user.create({ data: req.body })
 
     // Se tudo der certo, enviamos o código HTTP
     // apropriado, no caso
@@ -41,9 +42,11 @@ controller.retrieveAll = async function(req, res) {
   try {
     // Recupera todos os registros de clientes, ordenados
     // pelo campo "name"
-    const result = await prisma.seller.findMany({
-      orderBy: [ { fullname: 'asc' } ]
-    })
+    const result = await prisma.user.findMany({
+      	  	  omit: { password: true },
+      	  	  orderBy: [ { fullname: 'asc' } ]
+    			})
+
 
     // HTTP 200: OK (implícito)
     res.send(result)
@@ -63,9 +66,10 @@ controller.retrieveOne = async function (req, res) {
   try {
     // Busca no banco de dados apenas o cliente indicado
     // pelo parâmetro "id"
-    const result = await prisma.seller.findUnique({
-      where: { id: Number(req.params.id) }
-    })
+    const result = await prisma.user.findUnique({
+            omit: { password: true },
+      	  where: { id: Number(req.params.id) }
+    		})
 
     // Encontrou ~> HTTP 200: OK (implícito)
     if(result) res.send(result)
@@ -94,7 +98,7 @@ controller.update = async function (req, res){
 
     //Busca o registro no banco de dados por seu id
     //e o atualiza com as informações que vieram em req.body
-    await prisma.seller.update({
+    await prisma.user.update({
       where: { id: Number(req.params.id)},
       data: req.body
     })
@@ -116,7 +120,7 @@ controller.update = async function (req, res){
 
 controller.delete = async function (req,res){
   try{
-    await prisma.seller.delete({
+    await prisma.user.delete({
       where: { id: Number(req.params.id)}
     })
     //Encontrou e excluiu ~>HTTP 204: No content
@@ -134,4 +138,59 @@ controller.delete = async function (req,res){
     else res.status(500).end()
   }
 }
+controller.login = async function (req, res) {
+  try {
+    // Busca o usuário no BD usando o valor dos campos
+    // "username" OU "email"
+    const user = await prisma.user.findFirst({
+      where: {
+        OR: [
+          { username: req.body?.username },
+          { email: req.body?.email }
+        ]
+      }
+    })
+
+    // Se o usuário não for encontrado, retorna
+    // HTTP 401: Unauthorized
+    if(! user) {
+      console.error('ERRO DE LOGIN: usuário não encontrado')
+      return res.status(401).end()
+    }
+
+    // Usuário encontrado, vamos conferir a senha
+    const passwordIsValid = await bcrypt.compare(req.body?.password, user.password)
+
+    // Se a senha estiver errada, retorna
+    // HTTP 401: Unauthorized
+    if(! passwordIsValid) {
+      console.error('ERRO DE LOGIN: senha inválida')
+      return res.status(401).end()
+    }
+
+    // Deleta o campo "password" do objeto "user" antes de usá-lo
+    // no token e no valor de retorno
+    if(user.password) delete user.password
+
+    // Usuário/email e senha OK, passamos ao procedimento de gerar o token
+    const token = jwt.sign(
+      user,                       // Dados do usuário
+      process.env.TOKEN_SECRET,   // Senha para criptografar o token
+      { expiresIn: '24h' }        // Prazo de validade do token
+    )
+
+    // Retorna o token e o usuário autenticado, com o status
+    // HTTP 200: OK (implícito)
+    res.send({ user, token })
+  }
+  catch(error) {
+    // Se algo de errado acontecer, cairemos aqui
+    // Nesse caso, vamos exibir o erro no console e enviar
+    // o código HTTP correspondente a erro do servidor
+    // HTTP 500: Internal Server Error
+    console.error(error)
+    res.status(500).end()
+  }
+}
+
 export default controller
