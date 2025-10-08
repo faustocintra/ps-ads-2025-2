fa5eef1ec9a6e91a6b372781ece9b015a03ed0bf
Prof. Fausto Cintra <professor@faustocintra.com.br>
2025-10-08 13:42:24 +0000

    (08/10) Integração de login entre back-end e front-end

--- back-end/src/app.js
+++ back-end/src/app.js
@@ -1,3 +1,7 @@
+// Carregando as variáveis de ambiente do arquivo.env
+import dotenv from 'dotenv'
+dotenv.config()
+
 import express, { json, urlencoded } from 'express'
 import cookieParser from 'cookie-parser'
 import logger from 'morgan'
@@ -5,6 +9,18 @@ import logger from 'morgan'
 import indexRouter from './routes/index.js'
 
 const app = express()
+
+// Configurando o CORS para aceitar requisições a partir
+// dos servidores configurados na variável de ambiente
+// ALLOWED_ORIGINS
+import cors from 'cors'
+
+console.log('ALLOWED_ORIGINS:', process.env.ALLOWED_ORIGINS.split(','))
+
+app.use(cors({
+  origin: process.env.ALLOWED_ORIGINS.split(','),
+  credentials: true   // Habilita o envio de cookies para o front-end
+}))
 
 app.use(logger('dev'))
 app.use(json())
@@ -14,6 +30,10 @@ app.use(cookieParser())
 app.use('/', indexRouter)
 
 /**************** ROTAS *******************/
+
+// Middleware de verificação de autorização
+import authMiddleware from './middleware/auth.js'
+app.use(authMiddleware)
 
 import customersRoute from './routes/customers.js'
 app.use('/customers', customersRoute)
