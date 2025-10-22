b59efa12e40e2d3efa281f52b4c9a6a71bcdada4
Prof. Fausto Cintra <professor@faustocintra.com.br>
2025-10-22 12:53:28 +0000

    (22/10) Implementação de token JWT em cookie

--- back-end/src/controllers/users.js
+++ back-end/src/controllers/users.js
@@ -181,9 +181,28 @@ controller.login = async function (req, res) {
       { expiresIn: '24h' }        // Prazo de validade do token
     )
 
-    // Retorna o token e o usuário autenticado, com o status
+    // Formamos o cookie para enviar ao front-end
+    res.cookie(process.env.AUTH_COOKIE_NAME, token, {
+      httpOnly: true,     // Torna o cookie inacessível para JavaScript
+      secure: true,       // O cookie só trafegará em HTTPS ou localhost
+      sameSite: 'None',
+      path: '/',
+      maxAge: 24 * 60 * 60 * 1000   // 24h
+    })
+
+    // Cookie não HTTP-only, acessível via JS no front-end
+    res.cookie('not-http-only', 'Este-cookie-NAO-eh-HTTP-Only', {
+      httpOnly: false,
+      secure: true,   // O cookie será criptografado em conexões https
+      sameSite: 'None',
+      path: '/',
+      maxAge: 24 * 60 * 60 * 100  // 24h
+    })
+
+    // Retorna APENAS o usuário autenticado com
     // HTTP 200: OK (implícito)
-    res.send({ user, token })
+    res.send({user})
+
   }
   catch(error) {
     // Se algo de errado acontecer, cairemos aqui
