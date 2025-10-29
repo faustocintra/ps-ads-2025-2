d00f0cad84b40236bfcd5938479e5399274a5306
ademirsdiasfatec <ademirsldias@gmail.com>
2025-10-29 13:44:36 +0000

    (29/10) Implementação da segurança de rotas no front-end

--- front-end/src/routes/AppRoutes.jsx
+++ front-end/src/routes/AppRoutes.jsx
@@ -1,4 +1,4 @@
-import { Routes, Route } from 'react-router-dom'
+/*import { Routes, Route } from 'react-router-dom'
 
 import Homepage from '../pages/Homepage'
 import LoginPage from '../pages/LoginPage'
@@ -8,6 +8,9 @@ import CustomersForm from '../pages/customers/CustomersForm'
 
 import CarsList from '../pages/cars/CarsList'
 import CarsForm from '../pages/cars/CarsForm'
+
+import UsersList from '../pages/users/UsersList'
+import UsersForm from '../pages/users/UsersForm'
 
 export default function AppRoutes() {
   return <Routes>
@@ -22,5 +25,43 @@ export default function AppRoutes() {
     <Route path="/cars/new" element={ <CarsForm /> } />
     <Route path="/cars/:id" element={ <CarsForm /> } />
 
+    <Route path="/users" element={ <UsersList /> } />
+    <Route path="/users/new" element={ <UsersForm /> } />
+    <Route path="/users/:id" element={ <UsersForm /> } />
+
   </Routes>
+}*/
+
+import { Routes, Route } from 'react-router-dom'
+
+
+import AuthGuard from './AuthGuard'
+
+
+import { routes, UserLevel } from './routes'
+
+
+export default function AppRoutes() {
+ return (
+   <Routes>
+     {
+       routes.map(route => {
+         let element
+         if(route.userLevel > UserLevel.ANY) {
+           element = <AuthGuard userLevel={route.userLevel}>
+             {route.element}
+           </AuthGuard>
+         }
+         else element = route.element
+        
+         return <Route
+           key={route.route}
+           path={route.route}
+           element={element}
+         />
+       })
+     }
+   </Routes>
+ )
 }
+
